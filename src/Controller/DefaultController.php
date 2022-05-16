79d0a83416a828a4ca3aa6f6d2287930292db998
Harxia <jonathan.matulla@gmail.com>
2022-05-16 13:48:00 +0200

    Add title home - default

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+// src/Controller/ProgramController.php
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    #[Route('/', name: 'app_index')]
+    public function index()
+    {
+        return $this->render('default/index.html.twig');
+    }
+}
